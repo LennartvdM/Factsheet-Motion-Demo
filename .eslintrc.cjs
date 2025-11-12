7b149e7f4a7e5b5cf7f2181e7aa3c8f01b0e185c
Lennart van der Molen <lennartvandermolen@gmail.com>
2025-11-12 13:08:36 +0100

    chore: bootstrap react vite scaffold

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {}
+};
